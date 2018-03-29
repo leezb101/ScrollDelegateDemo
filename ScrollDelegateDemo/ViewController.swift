60942a152103775956197be1cef1bea30a8ae280
liyq5 <liyq5@asiainfo.com>
2018-03-29 17:24:16 +0800

    Initial Commit

---
+++ ScrollDelegateDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ScrollDelegateDemo
+//
+//  Created by leezb101 on 2018/3/29.
+//  Copyright © 2018年 luohe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
